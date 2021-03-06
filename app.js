42631013c14dea5cf08e42d979de4e43409296f6
Fatih <77235421+fatihcnn@users.noreply.github.com>
2021-03-06 10:18:58 +0300

    page optimized

--- app.js
+++ app.js
@@ -2,22 +2,44 @@ const modelContainer = document.querySelector(".sketchfab-embed-wrapper");
 const backBtn = document.querySelector(".model-backbtn-container");
 const replayBtn = document.querySelector(".replaybtn");
 const pcReplay = document.querySelector(".pc-replay-link");
+const messageInput = document.getElementById("message");
+const usernameInput = document.querySelector(".set-input");
+const sendButton = document.getElementById("sendBtn");
+const saveBtn = document.querySelector(".set-button");
 
-function mobile3dview() {
-  modelContainer.style.display = "flex";
-  modelContainer.style.position = "fixed";
-  modelContainer.style.top = "0";
-  modelContainer.style.left = "0";
-  document.body.style.overflow = "hidden";
-  backBtn.style.display = "block";
+function check() {
+  function inputClick() {
+    document.location = "#last";
+  }
+
+  function lmbtn() {}
+
+  if (localStorage.getItem("inputVisible") == "true") {
+    messageInput.style.display = "block";
+    sendButton.style.display = "block";
+    usernameInput.style.display = "none";
+    saveBtn.style.display = "none";
+  } else {
+    messageInput.style.display = "none";
+    sendButton.style.display = "none";
+  }
 }
-function mobile3dviewBack() {
-  backBtn.style.display = "none";
-  modelContainer.style.display = "none";
-  document.body.style.overflow = "visible";
-  backBtn.style.position = "fixed";
-  backBtn.style.top = "10";
-  backBtn.style.left = "0";
+
+function saveUsername() {
+  if (usernameInput.value === "") {
+    alert("Please set your username");
+  }
+
+  if (usernameInput.value != "fatihcan") {
+    messageInput.style.display = "block";
+    sendButton.style.display = "block";
+    localStorage.setItem("username", usernameInput.value);
+    localStorage.setItem("inputVisible", "true");
+    usernameInput.style.display = "none";
+    saveBtn.style.display = "none";
+  } else {
+    alert("you can not take admins username");
+  }
 }
 
 function sendMessage() {
@@ -42,46 +64,6 @@ function sended() {
   document.location = "#last";
 }
 
-const messageInput = document.getElementById("message");
-const usernameInput = document.querySelector(".set-input");
-const sendButton = document.getElementById("sendBtn");
-const saveBtn = document.querySelector(".set-button");
-
-function saveUsername() {
-  if (usernameInput.value === "") {
-    alert("Please set your username");
-  }
-
-  if (usernameInput.value != "fatihcan") {
-    messageInput.style.display = "block";
-    sendButton.style.display = "block";
-    localStorage.setItem("username", usernameInput.value);
-    localStorage.setItem("inputVisible", "true");
-    usernameInput.style.display = "none";
-    saveBtn.style.display = "none";
-  } else {
-    alert("you can not take admins username");
-  }
-}
-
-function check() {
-  function inputClick() {
-    document.location = "#last";
-  }
-
-  function lmbtn() {}
-
-  if (localStorage.getItem("inputVisible") == "true") {
-    messageInput.style.display = "block";
-    sendButton.style.display = "block";
-    usernameInput.style.display = "none";
-    saveBtn.style.display = "none";
-  } else {
-    messageInput.style.display = "none";
-    sendButton.style.display = "none";
-  }
-}
-
 firebase
   .database()
   .ref("votes")
@@ -101,12 +83,6 @@ function upClick() {
     });
 }
 
-function closeTab() {
-  document.querySelector(".iframee").src =
-    "https://sketchfab.com/models/5a708af84ef440c6a91f96c2b8284cc4/embed?autospin=0&autostart=0&preload=1&ui_theme=1";
-  document.querySelector(".last-flight2").style.display = "none";
-}
-
 firebase
   .database()
   .ref("messages")
@@ -124,7 +100,31 @@ firebase
     document.getElementById("messages").innerHTML += html;
   });
 
+function mobile3dview() {
+  modelContainer.style.display = "flex";
+  modelContainer.style.position = "fixed";
+  modelContainer.style.top = "0";
+  modelContainer.style.left = "0";
+  document.body.style.overflow = "hidden";
+  backBtn.style.display = "block";
+}
+function mobile3dviewBack() {
+  backBtn.style.display = "none";
+  modelContainer.style.display = "none";
+  document.body.style.overflow = "visible";
+  backBtn.style.position = "fixed";
+  backBtn.style.top = "10";
+  backBtn.style.left = "0";
+}
+
+function closeTab() {
+  document.querySelector(".iframee").src =
+    "https://sketchfab.com/models/5a708af84ef440c6a91f96c2b8284cc4/embed?autospin=0&autostart=0&preload=1&ui_theme=1";
+  document.querySelector(".last-flight2").style.display = "none";
+}
+
 function sn10Flight() {
+  window.location.href = "#";
   if (
     document.querySelector(".iframee").src !=
     "https://sketchfab.com/models/5a708af84ef440c6a91f96c2b8284cc4/embed?autospin=1&autostart=1&preload=1&ui_theme=1"
@@ -132,11 +132,8 @@ function sn10Flight() {
     document.querySelector(".iframee").src =
       "https://sketchfab.com/models/5a708af84ef440c6a91f96c2b8284cc4/embed?autospin=1&autostart=1&preload=1&ui_theme=1";
   }
-
   pcReplay.href = "https://www.youtube.com/watch?v=ODY6JWzS8WU";
   replayBtn.href = "https://www.youtube.com/watch?v=ODY6JWzS8WU";
-
-  window.location.href = "#";
   document.querySelector(".last-flight2").style.display = "flex";
   document.querySelector(".last-flight-name2").innerHTML =
     "Starship <span class='serial-number'>SN10</span>";
@@ -155,6 +152,7 @@ function sn10Flight() {
 }
 
 function sn9Flight() {
+  window.location.href = "#";
   if (
     document.querySelector(".iframee").src !=
     "https://sketchfab.com/models/5a708af84ef440c6a91f96c2b8284cc4/embed?autospin=1&autostart=1&preload=1&ui_theme=1"
@@ -162,11 +160,8 @@ function sn9Flight() {
     document.querySelector(".iframee").src =
       "https://sketchfab.com/models/5a708af84ef440c6a91f96c2b8284cc4/embed?autospin=1&autostart=1&preload=1&ui_theme=1";
   }
-
   replayBtn.href = "https://www.youtube.com/watch?v=_zZ7fIkpBgs&t=342s";
   pcReplay.href = "https://www.youtube.com/watch?v=_zZ7fIkpBgs&t=342s";
-
-  window.location.href = "#";
   document.querySelector(".last-flight2").style.display = "flex";
   document.querySelector(".last-flight-name2").innerHTML =
     "Starship <span class='serial-number'>SN9</span>";
@@ -184,6 +179,7 @@ function sn9Flight() {
 }
 
 function sn8Flight() {
+  window.location.href = "#";
   if (
     document.querySelector(".iframee").src !=
     "https://sketchfab.com/models/5a708af84ef440c6a91f96c2b8284cc4/embed?autospin=1&autostart=1&preload=1&ui_theme=1"
@@ -191,25 +187,18 @@ function sn8Flight() {
     document.querySelector(".iframee").src =
       "https://sketchfab.com/models/5a708af84ef440c6a91f96c2b8284cc4/embed?autospin=1&autostart=1&preload=1&ui_theme=1";
   }
-
   replayBtn.href = "https://www.youtube.com/watch?v=ap-BkkrRg-o";
   pcReplay.href = "https://www.youtube.com/watch?v=ap-BkkrRg-o";
-  window.location.href = "#";
   document.querySelector(".last-flight2").style.display = "flex";
-
   document.querySelector(".last-flight-name2").innerHTML =
     "Starship <span class='serial-number'>SN8</span>";
-
   document.querySelector(".last-flight-description2").innerHTML =
     "high-altitude flight test";
-
   document.querySelector(".last-flight-status2").style.backgroundColor =
     "#c20000";
   document.querySelector(".curret-status").innerHTML = "DESTROYED ON LANDING";
-
   document.querySelector(".last-flight-paragraph2").innerHTML =
     "Three Raptor engines, SN30, SN36, and SN42. The vehicle successfully launched, ascended, performed the skydive descent maneuver, relit the engines fueled by the header tanks, and steered to the landing pad. Low pressure in the fuel header tank caused engine issues, resulting in a hard landing and destruction of SN8. However, Elon Musk stated that all the vital telemetry data was acquired from the flight and he seemed pleased with the outcome";
-
   document.querySelector(".date2").innerHTML = "9 December 2020 22:45";
   document.querySelector(".launch-site2").innerHTML = "Boca Chica, Texas";
   document.querySelector(".apogee2").innerHTML = "12.5 km (7.8 mi)";
@@ -217,6 +206,7 @@ function sn8Flight() {
 }
 
 function sn6Flight() {
+  window.location.href = "#";
   if (
     document.querySelector(".iframee").src !=
     "https://sketchfab.com/models/5a708af84ef440c6a91f96c2b8284cc4/embed?autospin=1&autostart=1&preload=1&ui_theme=1"
@@ -226,22 +216,16 @@ function sn6Flight() {
   }
   replayBtn.href = "https://www.youtube.com/watch?v=MdAKrzOLQTg";
   pcReplay.href = "https://www.youtube.com/watch?v=MdAKrzOLQTg";
-  window.location.href = "#";
   document.querySelector(".last-flight2").style.display = "flex";
-
   document.querySelector(".last-flight-name2").innerHTML =
     "Starship <span class='serial-number'>SN6</span>";
-
   document.querySelector(".last-flight-description2").innerHTML =
     "150m Flight Test";
-
   document.querySelector(".last-flight-status2").style.backgroundColor =
     "#03ad00";
   document.querySelector(".curret-status").innerHTML = "SUCCESS";
-
   document.querySelector(".last-flight-paragraph2").innerHTML =
     "Single Raptor engine, SN29. A successful test hop took place on 3 September 2020";
-
   document.querySelector(".date2").innerHTML = "3 September 2020 17:47";
   document.querySelector(".launch-site2").innerHTML = "Boca Chica, Texas";
   document.querySelector(".apogee2").innerHTML = "150 m (490 ft)";
@@ -249,6 +233,7 @@ function sn6Flight() {
 }
 
 function sn5Flight() {
+  window.location.href = "#";
   if (
     document.querySelector(".iframee").src !=
     "https://sketchfab.com/models/5a708af84ef440c6a91f96c2b8284cc4/embed?autospin=1&autostart=1&preload=1&ui_theme=1"
@@ -258,22 +243,16 @@ function sn5Flight() {
   }
   replayBtn.href = "https://www.youtube.com/watch?v=s1HA9LlFNM0";
   pcReplay.href = "https://www.youtube.com/watch?v=s1HA9LlFNM0";
-  window.location.href = "#";
   document.querySelector(".last-flight2").style.display = "flex";
-
   document.querySelector(".last-flight-name2").innerHTML =
     "Starship <span class='serial-number'>SN5</span>";
-
   document.querySelector(".last-flight-description2").innerHTML =
     "150m Flight Test";
-
   document.querySelector(".last-flight-status2").style.backgroundColor =
     "#03ad00";
   document.querySelector(".curret-status").innerHTML = "SUCCESS";
-
   document.querySelector(".last-flight-paragraph2").innerHTML =
     "Single Raptor engine, SN27. A successful 150-meter flight was completed on 4 August 2020.";
-
   document.querySelector(".date2").innerHTML = "4 August 2020 23:57";
   document.querySelector(".launch-site2").innerHTML = "Boca Chica, Texas";
   document.querySelector(".apogee2").innerHTML = "150 m (490 ft)";
@@ -281,6 +260,7 @@ function sn5Flight() {
 }
 
 function sh3Flight() {
+  window.location.href = "#";
   if (
     document.querySelector(".iframee").src !=
     "https://sketchfab.com/models/3a14e78b7edb4e3baaa40069cdd60a7f/embed?autospin=1&autostart=1&preload=1&ui_theme=1"
@@ -290,21 +270,15 @@ function sh3Flight() {
   }
   replayBtn.href = "https://www.youtube.com/watch?v=bYb3bfA6_sQ";
   pcReplay.href = "https://www.youtube.com/watch?v=bYb3bfA6_sQ";
-  window.location.href = "#";
   document.querySelector(".last-flight2").style.display = "flex";
-
   document.querySelector(".last-flight-name2").innerHTML = "Starhopper";
-
   document.querySelector(".last-flight-description2").innerHTML =
     "150 Meter Starhopper Test";
-
   document.querySelector(".last-flight-status2").style.backgroundColor =
     "#03ad00";
   document.querySelector(".curret-status").innerHTML = "SUCCESS";
-
   document.querySelector(".last-flight-paragraph2").innerHTML =
     "Single Raptor engine, SN6. Starhopper was retired after this launch, with some parts being reused for other tests.";
-
   document.querySelector(".date2").innerHTML = "27 August 2019 22:00";
   document.querySelector(".launch-site2").innerHTML = "Boca Chica, Texas";
   document.querySelector(".apogee2").innerHTML = "150 m (490 ft)";
@@ -312,6 +286,7 @@ function sh3Flight() {
 }
 
 function sh2Flight() {
+  window.location.href = "#";
   if (
     document.querySelector(".iframee").src !=
     "https://sketchfab.com/models/3a14e78b7edb4e3baaa40069cdd60a7f/embed?autospin=1&autostart=1&preload=1&ui_theme=1"
@@ -321,21 +296,15 @@ function sh2Flight() {
   }
   replayBtn.href = "https://www.youtube.com/watch?v=bYb3bfA6_sQ";
   pcReplay.href = "https://www.youtube.com/watch?v=bYb3bfA6_sQ";
-  window.location.href = "#";
   document.querySelector(".last-flight2").style.display = "flex";
-
   document.querySelector(".last-flight-name2").innerHTML = "Starhopper";
-
   document.querySelector(".last-flight-description2").innerHTML =
     "20 Meter Starhopper Test";
-
   document.querySelector(".last-flight-status2").style.backgroundColor =
     "#03ad00";
   document.querySelector(".curret-status").innerHTML = "SUCCESS";
-
   document.querySelector(".last-flight-paragraph2").innerHTML =
     "First untethered flight test. Single Raptor engine, SN6.";
-
   document.querySelector(".date2").innerHTML = "25 July 2019";
   document.querySelector(".launch-site2").innerHTML = "Boca Chica, Texas";
   document.querySelector(".apogee2").innerHTML = "20 m (66 ft)";
@@ -343,6 +312,7 @@ function sh2Flight() {
 }
 
 function sh1Flight() {
+  window.location.href = "#";
   if (
     document.querySelector(".iframee").src !=
     "https://sketchfab.com/models/3a14e78b7edb4e3baaa40069cdd60a7f/embed?autospin=1&autostart=1&preload=1&ui_theme=1"
@@ -350,25 +320,17 @@ function sh1Flight() {
     document.querySelector(".iframee").src =
       "https://sketchfab.com/models/3a14e78b7edb4e3baaa40069cdd60a7f/embed?autospin=1&autostart=1&preload=1&ui_theme=1";
   }
-
   replayBtn.href = "https://www.youtube.com/watch?v=bYb3bfA6_sQ";
   pcReplay.href = "https://www.youtube.com/watch?v=bYb3bfA6_sQ";
-  window.location.href = "#";
-
   document.querySelector(".last-flight2").style.display = "flex";
-
   document.querySelector(".last-flight-name2").innerHTML = "Starhopper";
-
   document.querySelector(".last-flight-description2").innerHTML =
     "Starhopper Test";
-
   document.querySelector(".last-flight-status2").style.backgroundColor =
     "#03ad00";
   document.querySelector(".curret-status").innerHTML = "SUCCESS";
-
   document.querySelector(".last-flight-paragraph2").innerHTML =
     "Tethered hop which hit tethered limits. With a single Raptor SN2 engine.";
-
   document.querySelector(".date2").innerHTML = "5 April 2019";
   document.querySelector(".launch-site2").innerHTML = "Boca Chica, Texas";
   document.querySelector(".apogee2").innerHTML = "~1 m (3 ft 3 in)";
